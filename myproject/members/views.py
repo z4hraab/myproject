ae2d8e8dd8bdedd3714024fa1e04eae9439dacc8
zahra beigzadeh <z4hramah@gmail.com>
2026-01-02 16:50:56 +0330

    the file has been updated

--- myproject/members/views.py
+++ myproject/members/views.py
@@ -2,8 +2,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def Members(request):
-    template=loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers=Member.objects.all().values()
+    template=loader.get_template('all_members.html')
+    context={
+      'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context,request))
 
