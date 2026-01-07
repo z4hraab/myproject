1951634e5aec7903cf3081c27d163ac6d0fdbc13
zahra beigzadeh <z4hramah@gmail.com>
2026-01-07 12:08:29 +0330

    master.html and main.html completed

--- myproject/members/urls.py
+++ myproject/members/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    path('main',views.main,name='main'),
     path('members/',views.Members,name='members'),
+    path('members/details/<int:id>',views.details,name='details'),
+    
 ]
