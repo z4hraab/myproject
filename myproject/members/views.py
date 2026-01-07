bf24e99321b0cd33816702779e3680c6cb80bb4e
zahra beigzadeh <z4hramah@gmail.com>
2026-01-07 12:08:29 +0330

    master.html and main.html completed

--- myproject/members/views.py
+++ myproject/members/views.py
@@ -8,7 +8,22 @@ def Members(request):
     mymembers=Member.objects.all().values()
     template=loader.get_template('all_members.html')
     context={
-      'mymembers': mymembers
+      'mymembers': mymembers,
     }
     return HttpResponse(template.render(context,request))
+
+def details(request,id):
+    mymember=Member.objects.get(id=id)
+    template=loader.get_template('details.html')
+    context={
+      'mymember': mymember,
+    }
+    return HttpResponse(template.render(context,request))
+
+def main(request):
+    template=loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+
+    
 
